ec3ad5d0e44f03b33414e69932561d68a878b4be
SoniaV <sonia.vidalc1@gmail.com>
2024-12-10 22:25:32 -0300

    first commit

---
+++ src/helpers/index.ts
@@ -0,0 +1,14 @@
+export function formatCurrency(amount: number) {
+    return new Intl.NumberFormat('en-US',{style: 'currency', currency: 'USD'}).format(amount)
+}
+
+export function formatDate(dateSt: string) : string {
+    const dateObj = new Date(dateSt)
+    const options: Intl.DateTimeFormatOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return new Intl.DateTimeFormat('es-ES', options).format(dateObj)
+}
